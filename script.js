a70bf86ba8406f46b536bbb5e4418b3f37887cd5
Chandan NL <cnl3475@gmail.com>
2022-12-19 18:35:29 +0530

    Animation done

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const btn = document.getElementById('scrl-to-btn');
+// const btn = document.getElementById('scrl-to-btn');
 
-btn.addEventListener('click', () => window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-}));
+// btn.addEventListener('click', () => window.scrollTo({
+//     top: 0,
+//     behavior: 'smooth',
+// }));
+
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));  
